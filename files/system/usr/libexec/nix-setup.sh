5edb245feec7f9329cb3f92fccf921f16de25ccf
Scott Thompson <69859+PostlMC@users.noreply.github.com>
2025-07-13 14:33:48 -0500

    Added error handling

--- files/system/usr/libexec/nix-setup.sh
+++ files/system/usr/libexec/nix-setup.sh
@@ -3,9 +3,16 @@ set -euo pipefail
 
 # User creation
 if [ ! -f /var/lib/nix/.users-created ]; then
-    groupadd -g 30000 nixbld
+    # Only create group if it doesn't exist
+    if ! getent group nixbld >/dev/null 2>&1; then
+        groupadd -g 30000 nixbld
+    fi
     for i in $(seq 1 10); do
-        useradd -c "Nix build user $i" -d /var/empty -g nixbld -G nixbld -M -N -r -s $(which nologin) -u $((30000 + i)) nixbld$i
+        USERNAME="nixbld$i"
+        # Only create user if it doesn't exist
+        if ! id -u "$USERNAME" >/dev/null 2>&1; then
+            useradd -c "Nix build user $i" -d /var/empty -g nixbld -G nixbld -M -N -r -s $(which nologin) -u $((30000 + i)) "$USERNAME"
+        fi
     done
     touch /var/lib/nix/.users-created
 fi
